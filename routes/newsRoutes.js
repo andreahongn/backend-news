0c51f106e07e7c4f6c5d877ec561ee26cf5164d6
teraclitos <tefans12@gmail.com>
2023-04-21 10:55:03 -0300

    corrijo paginacion

--- routes/newsRoutes.js
+++ routes/newsRoutes.js
@@ -41,7 +41,10 @@ router
     const limit = req.query.limit;
     const page = req.query.page;
     try {
-      const allnews = await NewsModel.paginate({ limit: limit, page: page });
+      const allnews = await NewsModel.paginate(
+        {},
+        { limit: limit, page: page }
+      );
       res.status(200).send(allnews);
     } catch (error) {
       res.status(400).json({ error: true, message: error });
