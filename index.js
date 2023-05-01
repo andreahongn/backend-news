746ce431909e495ceaf793bfbb315409a474b896
teraclitos <tefans12@gmail.com>
2023-05-01 09:56:44 -0300

    borro comentarios página de inicio

--- index.js
+++ index.js
@@ -17,58 +17,9 @@ const routes = require("./routes");
 app.use("/", routes);
 
 app.use((req, res) => {
-  res.status(404).send("Page not found");
+  res.status(404).send("Página no encontrada");
 });
 
 app.listen(3001, () => {
   console.log("Server encendido en puerto 3001");
 });
-
-//Routes
-
-// app.use((req, res, next) => {
-//   res.status(404).json({ msg: "Pagina no encontrada" });
-// });
-
-// app.post(
-//   "/register",
-//   [
-//     body("user", "Campo usuario es requerido").notEmpty(),
-//     body("user", "Email invalido").isEmail(),
-//     body(
-//       "password",
-//       "Contraseña: mín 8 caracateres y max 25 caracteres"
-//     ).isLength({ min: 8, max: 25 }),
-//   ],
-//   async (req, res) => {}
-// );
-
-// app.get("/verusuarios", async (req, res) => {
-//   // const usuarios = await UserModel.find();
-//   // res.send(usuarios);
-//   res.status(200).json({ test: true });
-// });
-
-// app.get("/verusuario/:id", async (req, res) => {
-//   const idParams = req.params.id;
-//   const usuario = await UserModel.findOne({ _id: idParams });
-//   res.send(usuario);
-// });
-
-// app.put("/editarusuario/:id", async (req, res) => {
-//   const usuarioEditado = await UserModel.findOneAndUpdate(
-//     { _id: req.params.id },
-//     { name: req.body.name },
-//     { new: true }
-//   );
-//   res.send(usuarioEditado);
-// });
-
-// app.delete("/borrarusuario/:id", async (req, res) => {
-//   try {
-//     await UserModel.findOneAndDelete({ _id: req.params.id });
-//     res.send("usuario Eliminado");
-//   } catch (error) {
-//     console.log("error", error);
-//   }
-// });
